325522ad51ceecbf2ce9222d9e26f3a44954d1ec
dylan <fudaocheng@aidrive-tech.com>
2025-05-19 16:52:15 +0800

    java示例代码

--- heartbeat/UdpServer.java
+++ heartbeat/UdpServer.java
@@ -4,28 +4,47 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+
+/**
+ * UDP 服务器
+ * 本类使用单线程的线程池实现一个简单的UDP服务器，用于接收客户端发送的心跳信息。
+ * 实现方法多种多样，需要根据具体的业务场景进行选择。
+ */
 
 @Slf4j
 @Component
 public class UdpServer {
 
-    private static final int PORT = 10002;
 
+    private static final int PORT = 10002;
+    private volatile boolean isRunning = true; // 标志变量
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @PostConstruct
-    public void start() {
-        new Thread(this::listen).start();
+    public void startServer() {
+        executor.submit(this::listenUdpPackets);
     }
 
-    private void listen() {
+    @PreDestroy
+    public void stopServer() {
+        isRunning = false;
+        executor.shutdown();
+        log.info("UDP服务器已停止");
+    }
+
+
+    private void listenUdpPackets() {
         try (DatagramSocket socket = new DatagramSocket(PORT)) {
-            log.info("UDP Server started on port " + PORT);
+            log.info("UDP Server started on port {}", PORT);
             byte[] buffer = new byte[102400];
-            while (true) {
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            while (isRunning) {
                 socket.receive(packet);
                 String message = new String(packet.getData(), 0, packet.getLength()).trim();
                 log.info("❤️ Heartbeat from: {} ", packet.getAddress());
